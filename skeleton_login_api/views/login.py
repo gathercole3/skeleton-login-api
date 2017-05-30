340f2a13445b18cb442c7a0468d1c59ba5691850
jacob gathercole <jacobgathercole@gmail.com>
2017-05-30 20:07:10 +0100

    added code to verify a token with google

--- skeleton_login_api/views/login.py
+++ skeleton_login_api/views/login.py
@@ -1,11 +1,19 @@
 from flask import request, Blueprint, jsonify, current_app
+from oauth2client import client, crypt
 
 login = Blueprint('login', __name__)
 
 @login.route("/verify_google_token", methods=['POST'])
 def verify_google_token():
-    return jsonify({
-        "app": current_app.config["APP_NAME"],
-        "status": "OK",
-        "headers": request.headers.to_list(),
-    })
+    json_data = request.json
+
+    if 'token' in json_data.keys():
+        try:
+            idinfo = client.verify_id_token(json_data["token"], current_app.config["GOOGLE_CLIENT_ID"])
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise crypt.AppIdentityError("Wrong issuer.")
+        except crypt.AppIdentityError:
+            raise ValueError('invalid missing')
+        return jsonify(idinfo)
+    else:
+      raise ValueError('token missing')
