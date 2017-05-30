0ded15044a4af4d96acb7b5e2566226617822dd7
jacob gathercole <jacobgathercole@gmail.com>
2017-05-30 22:48:56 +0100

    added inital databse migration

--- skeleton_login_api/models.py
+++ skeleton_login_api/models.py
@@ -1,21 +1,8 @@
 from skeleton_login_api import db
-from sqlalchemy import ForeignKey
 from sqlalchemy.orm.session import Session
 
-class Test_table_1(db.Model):
+class users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64))
+    email = db.Column(db.String(64))
     password = db.Column(db.String(64))
-    info = db.Column(db.String(64))
-
-class Test_table_2(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    col_1 = db.Column(db.String(64))
-    col_2 = db.Column(db.String(64))
-    col_3 = db.Column(db.String(64))
-
-class Test_table_3(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    col_1 = db.Column(db.String(64))
-    col_2 = db.Column(db.String(64))
-    col_3 = db.Column(db.String(64))
+    google_id = db.Column(db.String(64))
